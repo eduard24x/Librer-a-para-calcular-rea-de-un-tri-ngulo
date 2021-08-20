4177cb7c0f7cc6642f2e81463a68b1db1122d1e6
eduard24x <71943757+eduard24x@users.noreply.github.com>
2021-08-20 03:19:03 -0500

    Add files via upload

---
+++ Creacion/src/lbarea/triangulo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lbarea;
+
+/**
+ *
+ * @author Eduard
+ */
+public class triangulo {
+    public static double funcionA(double x, double y){
+        return (x*y)/2;
+    }
+    
+}
